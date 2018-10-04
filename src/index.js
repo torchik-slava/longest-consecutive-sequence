0bad0576c7e7918686813378d3678cc129b33683
Slava Torchik <torhik_slava@mail.ru>
2018-10-04 23:25:51 +0300

    longest consecutive sequence/solution: O(n) or not O(n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  const hashTable = {};
+  let count = 0, max = 0;
+  for (let i=0; i<array.length; i++){
+    hashTable[array[i]]=array[i]+1;
+  }
+  const repeat = (num) => {
+    if (hashTable[num]===undefined) {
+      if(count>max){max=count}
+      count=0;      
+    } else {
+      count++;
+      repeat(hashTable[num]);
+    }
+  }
+
+  for (let i=0; i<array.length; i++){
+    repeat(array[i]);
+  }
+  return max;
 }
